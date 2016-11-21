1a1494aa4fd9d8f437cf53eeec754a2a43457ce2
t1t1 <cha@hanmail2.net>
2016-11-21 13:40:04 +0900

    init

---
+++ src/main/java/com/example/Emp.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Emp {
+}
---
+++ src/main/java/com/example/SpringBootJpaDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaDemoApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaDemoApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+	}
+}
